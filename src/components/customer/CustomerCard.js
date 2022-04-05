47769283810cde2f4ac418dc0299991cf6cdf070
jandershock <jordan.andershock@gmail.com>
2022-04-05 14:35:54 -0500

    initializing

---
+++ src/components/customer/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Customer.css"
+
+export const CustomerCard = () => (
+    <section className="customer">
+        <h3 className="customer__name">Bob</h3>
+        <div className="customer__address">Bob's House</div>
+    </section>
+)
---
+++ src/components/employees/EmployeeCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Employee.css"
+
+export const EmployeeCard = () => {
+    
+    return (
+    <section className="employee">
+        <h3 className="employee__name">John</h3>
+        <div className="employee__address">John's House</div>
+    </section>
+)}
